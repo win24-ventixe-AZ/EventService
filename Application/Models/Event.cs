9610ac993a7bf050a0c48ecf3f7959804b913913
AdrianZafari <adrian006@hotmail.com>
2025-06-09 00:06:41 +0200

    cleanup

--- Application/Models/Event.cs
+++ Application/Models/Event.cs
@@ -10,7 +10,7 @@ public class Event
     public DateTime EventDate { get; set; } // includes the time of the event
     public DateTime TimeCreated { get; set; } 
     public string? Type { get; set; } = "Unknown";
-    public string? Status { get; set; } = "Draft";
+    public string? Status { get; set; }
     public string? Location { get; set; }
     public string? Description { get; set; }
     public decimal? Price { get; set; } // This is here to simplify the backend processes for now
